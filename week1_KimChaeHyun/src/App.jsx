4411f9fcf1af8546161ce82ad45626a8b88f142d
imddoy <imddoy@duksung.ac.kr>
2024-10-31 15:29:49 +0900

    init: 초기 세팅

---
+++ week1_KimChaeHyun/src/App.jsx
@@ -0,0 +1,18 @@
+import { RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import theme from "@styles/theme";
+import GlobalStyle from "@styles/global";
+import router from "@routes/Router";
+
+function App() {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ week1_KimChaeHyun/src/assets/svgs/Notification.jsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+const SvgNotification = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+    <path
+      fill="currentColor"
+      d="M4 19v-2h2v-7q0-2.074 1.25-3.688Q8.5 4.7 10.5 4.2v-.7q0-.624.438-1.063A1.45 1.45 0 0 1 12 2q.624 0 1.063.438.437.437.437 1.062v.7q2 .5 3.25 2.112Q18 7.926 18 10v7h2v2zm8 3q-.825 0-1.412-.587A1.93 1.93 0 0 1 10 20h4q0 .824-.588 1.413A1.93 1.93 0 0 1 12 22m-4-5h8v-7q0-1.65-1.175-2.825T12 6c-1.65 0-2.042.392-2.825 1.175Q8 8.35 8 10z"
+    />
+  </svg>
+);
+export default SvgNotification;
